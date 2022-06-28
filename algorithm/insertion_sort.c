35e0ce081a0955a4bf3fd8e14dfc8462108522ce
chahyeonnaa <gusdk21650@naver.com>
2022-06-28 11:39:28 +0900

    insertion sort

---
+++ algorithm/insertion_sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+    int array[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+    int temp, j;
+
+    for (int i = 0; i < 9; i++)
+    {
+        j = i;
+        while (array[j] > array[j + 1])
+        {
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+            j--;
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", array[i]);
+    }
+}
